c5a06833e33a4516b12d6995ce20ed842479aca3
Vladislav Gerasimov <gerasimov.v88@gmail.com>
2019-12-02 15:50:26 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Information
+This project is a sample of using database migrator such as **Liquibase** 
+for creating standalone application-migrator.
---
+++ src/main/java/ru/vladigeras/liquibasemigrator/LiquibaseMigratorDemo.java
@@ -0,0 +1,10 @@
+package ru.vladigeras.liquibasemigrator;
+
+/**
+ * @author vladi_geras on 02.12.2019
+ */
+public class LiquibaseMigratorDemo {
+	public static void main(String[] args) {
+
+	}
+}
