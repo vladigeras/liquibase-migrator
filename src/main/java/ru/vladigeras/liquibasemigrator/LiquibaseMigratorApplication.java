207d2e5094c99199e7170793d4b76b2769ed36c1
Vladislav Gerasimov <gerasimov.v88@gmail.com>
2019-12-03 13:03:45 +0300

    Examples of adding some changesets with table creational and data inserting

---
+++ src/main/java/ru/vladigeras/liquibasemigrator/LiquibaseMigratorApplication.java
@@ -0,0 +1,14 @@
+package ru.vladigeras.liquibasemigrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author vladi_geras on 02.12.2019
+ */
+@SpringBootApplication
+public class LiquibaseMigratorApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseMigratorApplication.class, args);
+	}
+}
